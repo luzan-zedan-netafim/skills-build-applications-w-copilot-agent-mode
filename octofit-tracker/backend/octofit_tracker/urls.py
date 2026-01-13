f6ef45cd165342ae7b8cb3ffbd5611645a2f0134
luzan-zedan-netafim <luzan.zedan@netafim.com>
2026-01-13 12:20:22 +0000

    update urls

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -1,5 +1,5 @@
 """octofit_tracker URL Configuration
-
+ 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
@@ -13,31 +13,44 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from .views import UserViewSet, TeamViewSet, ActivityViewSet, LeaderboardViewSet, WorkoutViewSet
-from rest_framework.response import Response
+from .views import UserViewSet, TeamViewSet, ActivityViewSet, WorkoutViewSet, LeaderboardViewSet
 from rest_framework.decorators import api_view
-
+from rest_framework.response import Response
+ 
+from rest_framework.reverse import reverse
+import os
+ 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'teams', TeamViewSet)
 router.register(r'activities', ActivityViewSet)
-router.register(r'leaderboard', LeaderboardViewSet)
 router.register(r'workouts', WorkoutViewSet)
-
+router.register(r'leaderboard', LeaderboardViewSet)
+ 
 @api_view(['GET'])
 def api_root(request, format=None):
-    return Response({
-        'users': request.build_absolute_uri('users/'),
-        'teams': request.build_absolute_uri('teams/'),
-        'activities': request.build_absolute_uri('activities/'),
-        'leaderboard': request.build_absolute_uri('leaderboard/'),
-        'workouts': request.build_absolute_uri('workouts/'),
-    })
-
+    codespace_name = os.environ.get('CODESPACE_NAME')
+    if codespace_name:
+        base_url = f'https://{codespace_name}-8000.app.github.dev/api/'
+        return Response({
+            'users': base_url + 'users/',
+            'teams': base_url + 'teams/',
+            'activities': base_url + 'activities/',
+            'workouts': base_url + 'workouts/',
+            'leaderboard': base_url + 'leaderboard/',
+        })
+    else:
+        return Response({
+            'users': request.build_absolute_uri(reverse('user-list')),
+            'teams': request.build_absolute_uri(reverse('team-list')),
+            'activities': request.build_absolute_uri(reverse('activity-list')),
+            'workouts': request.build_absolute_uri(reverse('workout-list')),
+            'leaderboard': request.build_absolute_uri(reverse('leaderboard-list')),
+        })
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
